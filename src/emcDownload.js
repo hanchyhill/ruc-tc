9cb2d53dc8b00b5d7434f18898f00740633530a9
minhill <lrrq369@gmail.com>
2019-08-19 15:41:21 +0800

    Update emcDownload.js

--- src/emcDownload.js
+++ src/emcDownload.js
@@ -10,9 +10,14 @@ const resolveTCFA = require('./lib/resolveTCFA.js').resolveTCFA;
 const {pMakeDir,isExists,readFile,writeFile} = require('./lib/util.js');
 const {connect,initSchemas} = require('./db/initDB.js');
 const schedule = require('node-schedule');
+dayjs.extend(toArray);
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+dayjs.extend(utc);
+dayjs.extend(customParseFormat);
 let config = {
   listUrl:'https://www.emc.ncep.noaa.gov/gmb/tpm/emchurr/tcgen/datebar.html',
   wpUrlPrefix:'https://www.emc.ncep.noaa.gov/gmb/tpm/emchurr/tcgen/atx/storms.aeperts.atcf_gen.wptg.',
+  fileName(date){return 'storms.aeperts.atcf_gen.wptg.'+date+'.txt'},
 }
 
 async function readLocal(){
@@ -30,7 +35,7 @@ function splitBul(str=''){
   newLines.forEach(line=>line.splice(2,1));
   let stormList = [];
   let stormName = [];
-  for(let line of newLines){
+  for(let line of newLines){// 按编号归类不同气旋
     let name = line[1];
     let index = stormName.findIndex(v=>v===name);
     if(index!==-1){
@@ -65,24 +70,48 @@ const getLatestList = async ()=>{
   let hrefList = $('a');
   let dateList = [];
   hrefList.each(function(i, elem) {
-    dateList.push($(this).text());
+    dateList.push($(this).text().trim());
   });
   dateList.shift();
   let leastLinks = dateList.slice(0,3);//获取前3个时次
-  return leastLinks.map(date=>config.wpUrlPrefix+date);
+  return leastLinks;
 }
 
 const main = async ()=>{
   let need2DownloadList = await getLatestList();
-  for(let url of need2DownloadList){
-    let rpOption = {uri: url,timeout: 40*1000,};
+  for(let date of need2DownloadList){
+    let rpOption = {uri: config.wpUrlPrefix + date,timeout: 40*1000,};
+    let initTime = dayjs.utc(date,'YYYYMMDDHH');
+    let dirPath = path.resolve(__dirname+'./../../data/cyclone/emc/ncep/',date.slice(0,4));
     // TODO 检查是否存在;
-    let bulletinRaw = await rp(rpOption)
-      .catch(err=>{
-        console.error(err);
-      });
+    let bulletinRaw;
+    let fileName = config.fileName(date);
+    try{
+      await pMakeDir(dirPath);//创建不存在的目录
+      const filePath = path.resolve(dirPath, fileName);
+      let isFileExists = await isExists(filePath);// 文件路径
+      if(!isFileExists){
+        try{
+          console.log('准备下载'+fileName);
+          bulletinRaw = await rp(rpOption);
+        }catch(err){
+          console.error('下载文件发生意外'+fileName);
+          throw err;
+        }
+        writeFile(filePath, bulletinRaw)
+          .catch(err=>{console.log(err)});
+      }else{
+        // myDebug(`文件已存在${RegArr[2]}`);
+        continue;
+      }
+    }
+    catch(err){
+      console.log(err);
+    };
+    let stormList = splitBul(bulletinRaw);
+    // console.log(stormList.length);
   }
 }
 
-getLatestList()
+main()
   .catch(err=>console.error(err));
